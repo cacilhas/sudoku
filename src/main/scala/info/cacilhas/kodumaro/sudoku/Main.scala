57a5b992ec1189d507350dcb89f66c9100b0cf57
Rodrigo Cacilhas <rodrigo.cacilhas@contabilone.com>
2020-08-19 18:31:48 -0300

    check for console-based sudoku before start

--- src/main/scala/info/cacilhas/kodumaro/sudoku/Main.scala
+++ src/main/scala/info/cacilhas/kodumaro/sudoku/Main.scala
@@ -1,10 +1,32 @@
 package info.cacilhas.kodumaro.sudoku
 
+import java.io.IOException
+
 import info.cacilhas.kodumaro.sudoku.ui.mainwindow.Window
-import javax.swing.UIManager
+import javax.swing.{JOptionPane, UIManager}
+
+import util.{Failure, Success, Try}
 
 object Main extends App {
 
+  import sys.process._
+
   UIManager setLookAndFeel UIManager.getSystemLookAndFeelClassName
-  new Window setVisible true
+
+  Try("sudoku -g"!!) match {
+    case Success(_) ⇒ new Window setVisible true
+
+    case Failure(exc) if exc.isInstanceOf[IOException] ⇒
+      JOptionPane showMessageDialog (
+        null,
+        """Console-based Sudoku not found!
+          |Please install the package.
+          |""".stripMargin,
+        "Kodumaro Sudoku",
+        JOptionPane.ERROR_MESSAGE,
+      )
+      System exit 1
+
+    case Failure(exc) ⇒ throw exc
+  }
 }
