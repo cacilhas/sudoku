c44cb4b033a3438e65c0bf9248fefa6c3defbb3b
Rodrigo Cacilhas <rodrigo.cacilhas@contabilone.com>
2020-08-19 10:38:10 -0300

    set components’ creation apart

---
+++ src/main/scala/info/cacilhas/kodumaro/sudoku/ui/mainwindow/ComponentsMixin.scala
@@ -0,0 +1,64 @@
+package info.cacilhas.kodumaro.sudoku.ui.mainwindow
+
+import java.awt.event.KeyEvent
+import java.awt.{Frame, Menu, MenuItem, MenuShortcut}
+
+import info.cacilhas.kodumaro.sudoku.game.ClassLevel
+import info.cacilhas.kodumaro.sudoku.ui.BoardCanvas
+
+private[mainwindow] trait ComponentsMixin {
+  this: Frame with ActionListenerMixin ⇒
+
+  import ClassLevel._
+  import KeyEvent._
+
+  protected def frmBoard: BoardCanvas
+  private val mnFile = new Menu("File")
+  private val mnNew = new Menu("New")
+  private val mnHelp = new Menu("Help")
+  private val itOpen = new MenuItem("Open")
+  private val itSave = new MenuItem("Save")
+  private val itQuit = new MenuItem("Quit")
+  private val itAbout = new MenuItem("About")
+  private val itEasy = new MenuItem(Easy.toString)
+  private val itMedium = new MenuItem(Medium.toString)
+  private val itHard = new MenuItem(Hard.toString)
+  private val itPro = new MenuItem(Pro.toString)
+
+  start(itEasy, "newEasy", VK_E)
+  start(itMedium, "newMedium", VK_N)
+  start(itHard, "newHard", VK_H)
+  start(itPro, "newPro", VK_P)
+  start(itOpen, "open", VK_O)
+  start(itSave, "save", VK_S)
+  start(itQuit, "quit", VK_Q)
+  start(itAbout, "about")
+
+  protected def packComponents(): Unit = {
+    mnNew setFont getFont
+    mnNew add itEasy
+    mnNew add itMedium
+    mnNew add itHard
+    mnNew add itPro
+
+    mnHelp add itAbout
+
+    mnFile setFont getFont
+    mnFile add mnNew
+    mnFile add itOpen
+    mnFile add itSave
+    mnFile addSeparator()
+    mnFile add itQuit
+
+    getMenuBar add mnFile
+    getMenuBar add mnHelp
+    add(frmBoard)
+  }
+
+  private def start(item: MenuItem, action: String, shortcut: Int = -1): Unit = {
+    item setFont getFont
+    item setActionCommand action
+    addActionListenerTo(item)
+    if (shortcut != -1) item setShortcut new MenuShortcut(shortcut)
+  }
+}
--- src/main/scala/info/cacilhas/kodumaro/sudoku/ui/mainwindow/Window.scala
+++ src/main/scala/info/cacilhas/kodumaro/sudoku/ui/mainwindow/Window.scala
@@ -3,7 +3,6 @@ package info.cacilhas.kodumaro.sudoku.ui.mainwindow
 import java.awt._
 import java.awt.event._
 
-import info.cacilhas.kodumaro.sudoku.game.ClassLevel
 import info.cacilhas.kodumaro.sudoku.model.Board
 import info.cacilhas.kodumaro.sudoku.ui.{BoardCanvas, Theme}
 import javax.swing._
@@ -13,27 +12,14 @@ final class Window extends JFrame
                    with BoardMixin
                    with ActionListenerMixin
                    with FileManagementMixin
-                   with WindowListenerMixin {
+                   with WindowListenerMixin
+                   with ComponentsMixin {
   window ⇒
-
-  import ClassLevel._
 
   private val theme = Theme(fg = Color.lightGray, bg = Color.black)
   private val dim = new Dimension(724, 800)
-
-  // UI components
+  private lazy val version = Option(getClass.getPackage.getImplementationVersion) getOrElse "1.0"
   override protected val frmBoard = new BoardCanvas(window, theme)
-  private val mnFile = new Menu("File")
-  private val mnNew = new Menu("New")
-  private val mnHelp = new Menu("Help")
-  private val itOpen = new MenuItem("Open")
-  private val itSave = new MenuItem("Save")
-  private val itQuit = new MenuItem("Quit")
-  private val itAbout = new MenuItem("About")
-  private val itEasy = new MenuItem(Easy.toString)
-  private val itMedium = new MenuItem(Medium.toString)
-  private val itHard = new MenuItem(Hard.toString)
-  private val itPro = new MenuItem(Pro.toString)
 
   setTitle("Kodumaro Sudoku")
   setSize(dim)
@@ -45,38 +31,8 @@ final class Window extends JFrame
   setMenuBar(new MenuBar)
   getContentPane setLayout new GridBagLayout
   setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
-
-  start(itEasy, "newEasy", KeyEvent.VK_E)
-  start(itMedium, "newMedium", KeyEvent.VK_N)
-  start(itHard, "newHard", KeyEvent.VK_H)
-  start(itPro, "newPro", KeyEvent.VK_P)
-  start(itOpen, "open", KeyEvent.VK_O)
-  start(itSave, "save", KeyEvent.VK_S)
-  start(itQuit, "quit", KeyEvent.VK_Q)
-  start(itAbout, "about")
-
-  mnNew setFont getFont
-  mnNew add itEasy
-  mnNew add itMedium
-  mnNew add itHard
-  mnNew add itPro
-
-  mnHelp add itAbout
-
-  mnFile setFont getFont
-  mnFile add mnNew
-  mnFile add itOpen
-  mnFile add itSave
-  mnFile addSeparator ()
-  mnFile add itQuit
-
-  getMenuBar add mnFile
-  getMenuBar add mnHelp
-
   board = Board()
-  add(frmBoard)
-
-  private lazy val version = Option(getClass.getPackage.getImplementationVersion) getOrElse "1.0"
+  packComponents()
 
   override def close(): Unit = dispatchEvent(new WindowEvent(window, WindowEvent.WINDOW_CLOSING))
 
@@ -94,11 +50,4 @@ final class Window extends JFrame
   }
 
   override protected def onBoardUpdate(): Unit = frmBoard.board = board
-
-  private def start(item: MenuItem, action: String, shortcut: Int = -1): Unit = {
-    item setFont getFont
-    item setActionCommand action
-    addActionListenerTo(item)
-    if (shortcut != -1) item setShortcut new MenuShortcut(shortcut)
-  }
 }
